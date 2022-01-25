2a2c6a228cc0f6fb1d07256f8a72acd32c287091
Afonso Bertoncini <bertoncinis@hotmail.com>
2022-01-25 14:52:39 -0300

    editando um heroi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule    // permite usar recuros como to e database que faz modificação interligada
   ],
   providers: [],
   bootstrap: [AppComponent]
