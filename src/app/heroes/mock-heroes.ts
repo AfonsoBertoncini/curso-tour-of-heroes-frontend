2a8859b0d232be50aaf73c40cd09eedf40135eff
Afonso Bertoncini <bertoncinis@hotmail.com>
2022-01-25 18:33:04 -0300

    ]

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+{ id: 1, name: 'shaco' },
+{ id: 2, name: 'singed' },
+{ id: 3, name: 'nasus' },
+{ id: 4, name: 'mir4draco' },
+{ id: 5, name: 'brand' },
+{ id: 6, name: 'zed' },
+{ id: 7, name: 'zac' },
+{ id: 8, name: 'fiddle' },
+];
